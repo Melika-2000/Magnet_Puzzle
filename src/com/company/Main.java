b3d877d4d7e74bb4c9d59ae33f2b367d60b9263a
MohMollaie <mohammad.mollaie.14@gmail.com>
2021-12-19 20:18:13 -0600

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,43 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("proj 3");
+
+    public static void main(String args[]){
+
+        Scanner scanner = new Scanner(System.in);
+        int rowNumber = scanner.nextInt();
+        int columnNumber = scanner.nextInt();
+        int[] posRowNumbers = new int[rowNumber];
+        int[] negRowNumbers = new int[rowNumber];
+        int[] posColNumbers = new int[columnNumber];
+        int[] negColNumbers = new int[columnNumber];
+        int[][] intTable = new int[rowNumber][columnNumber];
+
+        for(int j = 0; j < rowNumber; j++){
+            posRowNumbers[j] = scanner.nextInt();
+        }
+
+        for(int j = 0; j < rowNumber; j++){
+            negRowNumbers[j] = scanner.nextInt();
+        }
+
+        for(int j = 0; j < columnNumber; j++){
+            posColNumbers[j] = scanner.nextInt();
+        }
+
+        for(int j = 0; j < columnNumber; j++){
+            negColNumbers[j] = scanner.nextInt();
+        }
+
+        for(int i = 0; i < rowNumber; i++){
+            for(int j = 0; j < columnNumber; j++){
+                intTable[i][j] = scanner.nextInt();
+            }
+        }
+
+        Table table = new Table(rowNumber,columnNumber,intTable);
     }
 }
---
+++ src/com/company/Table.java
@@ -0,0 +1,36 @@
+package com.company;
+public class Table {
+    int RowNums;
+    int ColNums;
+
+    Piece[][] tableMap = new Piece[RowNums][ColNums];
+
+    public Table(int rowNums, int colNums, int[][] intTable) {
+        this.RowNums = rowNums;
+        this.ColNums = colNums;
+
+        for(int i = 0; i < rowNums; i++){
+            for(int j = 0; j < colNums; j++){
+                this.tableMap[i][j].setMagnet(false);
+            }
+        }
+
+        for(int i = 0; i < rowNums; i++){
+            for(int j = 0; j < colNums; j++){
+                if(i > 0 && intTable[i - 1][j] == intTable[i][j]){
+                    this.tableMap[i][j] = new Piece(i,j,i-1,j);
+                }
+                if(i < rowNums - 1 && intTable[i + 1][j] == intTable[i][j]){
+                    this.tableMap[i][j] = new Piece(i,j,i+1,j);
+                }
+                if(j > 0 && intTable[i][j - 1] == intTable[i][j]){
+                    this.tableMap[i][j] = new Piece(i,j,i,j-1);
+                }
+                if(j < colNums - 1 && intTable[i][j + 1] == intTable[i][j]){
+                    this.tableMap[i][j] = new Piece(i,j,i,j+1);
+                }
+            }
+        }
+
+    }
+}
